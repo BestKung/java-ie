3ef5125069fafe67b6dfb8570575d45649c8c7af
Bestkung <best.kittiporn@gmail.com>
2016-01-28 14:04:01 -0800

    commit first

---
+++ src/th/co/best/server/ServerTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.co.best.server;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author BestKung
+ */
+public class ServerTest {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(3333);
+        Socket socket = null;
+
+        try {
+            while (true) {
+                socket = serverSocket.accept();
+                System.out.println("Client Accept.!");
+                BufferedReader fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+
+                String fileName = (fromClient.readLine()).substring(3);
+                System.out.println(fileName);
+                System.out.println("Accept filenname \n");
+                outputStream.writeBytes("Request Complete \n");
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                fileName = "F:\\"+fileName;
+                System.out.println("Before file"+fileName);
+                File file = new File(fileName);
+                DataOutputStream outputStream1 = new DataOutputStream(new FileOutputStream(file));
+
+                byte[] buffer = new byte[4096];
+                int len = -1;
+                while ((len = dataInputStream.read(buffer)) != -1) {
+                    outputStream1.write(buffer, 0, len);
+                }
+            }
+        } catch (Exception e) {
+        } finally {
+            socket.close();
+            
+        }
+    }
+}
---
+++ src/th/co/best/server/client/ClientTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.co.best.server.client;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ *
+ * @author BestKung
+ */
+public class ClientTest {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 3333);
+        String modifierSentence = null;
+        while (true) {
+
+            String fileName = "";
+            BufferedReader bufferedReader = null;
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            System.out.println("Upload File.");
+
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Input Path File : ");
+            fileName = scanner.nextLine();
+
+            System.out.println(fileName);
+            dataOutputStream.writeBytes(fileName + "\n");
+            modifierSentence = bufferedReader.readLine();
+            System.out.println("From Server" + modifierSentence);
+
+            File file = new File(fileName);
+            System.out.println(file);
+            System.out.println("th.co.best.server.client.ClientTest.main()");
+            DataInputStream dataInputStream = new DataInputStream(new FileInputStream(file));
+
+            System.out.println("th.co.best.server.client.ClientTest.main()");
+            System.out.println();
+            byte[] buffer = new byte[512];
+            int len = -1;
+            while ((len = dataInputStream.read(buffer)) != -1) {
+                dataOutputStream.write(buffer, 0, len);
+            }
+        }
+    }
+}
